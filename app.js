9c9280ea3e0210ce154b135085a3c36d84de82a6
Dave <davejohn441010@gmail.com>
2022-10-06 12:26:50 +0800

    added info card for mobile devices

--- app.js
+++ app.js
@@ -7,14 +7,59 @@ const submitQuery = async (searchTerm) => {
     return searchResults
 }
 
+const createInfoElements = (showTitle, showRating, showSummary) => {
+    const overlay = document.createElement('div')
+    overlay.classList.add('overlay')
+    const overlayContentContainer = document.createElement('div')
+    overlayContentContainer.classList.add('overlay-content-container')
+    const movieTitle = document.createElement('h1')
+    movieTitle.classList.add('movie-title')
+    movieTitle.innerHTML = showTitle
+    const rating = document.createElement('h2')
+    rating.classList.add('rating')
+    const movieRating = document.createElement('span')
+    movieRating.classList.add('movie-rating')
+    if (showRating === null) showRating = 0
+    movieRating.innerHTML = showRating
+    const starSymbol = document.createElement('i')
+    starSymbol.classList.add('fa-solid')
+    starSymbol.classList.add('fa-star')
+    const starSymbol1 = document.createElement('i')
+    starSymbol1.classList.add('fa-solid')
+    starSymbol1.classList.add('fa-star')
+    const overTen = document.createTextNode('/ 10')
+    const movieSummary = document.createElement('p')
+    movieSummary.classList.add('movie-summary')
+    movieSummary.innerHTML = showSummary
+
+    overlay.append(overlayContentContainer)
+    overlayContentContainer.append(movieTitle)
+    overlayContentContainer.append(rating)
+    rating.append(movieRating)
+    rating.append(starSymbol)
+    rating.append(overTen)
+    rating.append(starSymbol1)
+    overlayContentContainer.append(movieSummary)
+
+    return overlay
+}
+
 const listQueries = (searchObj) => {
+    let loadImages = 0
     for (let e of searchObj) {
         const newShow = document.createElement('div')
         const showImg = document.createElement('img')
         newShow.classList.add('movie-card')
         showImg.src = e.show.image.original
+        let smallInfoOverlay = createInfoElements(e.show.name, e.show.rating.average, e.show.summary)
+        newShow.append(smallInfoOverlay)
+        console.log(newShow)
         newShow.append(showImg)
         resultCont.append(newShow)
+
+        newShow.addEventListener('click', () => {
+            newShow.classList.toggle('small-screen-menu')
+        })
     }
 }
 
